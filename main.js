3680edd5bae4ceb500e2670996b956e36927bfac
Ahmed Kamel Taha <ahmedengu@gmail.com>
2016-12-25 22:22:22 +0200

    signup , profile , edit profile

--- main.js
+++ main.js
@@ -16,6 +16,9 @@ app.config(function ($routeProvider) {
     }).when("/profile", {
         templateUrl: "views/profile.html",
         controller: "profileCtrl"
+    }).when("/editProfile", {
+        templateUrl: "views/editProfile.html",
+        controller: "editProfileCtrl"
     }).when("/services", {
         templateUrl: "views/services.html",
         controller: "servicesCtrl"
@@ -40,10 +43,15 @@ app.run(function ($rootScope, $location) {
     $rootScope.logout = function () {
         showSpinner();
         Parse.User.logOut().then(function () {
-            location.href = ".#/";
+            location.href = ".#!/";
             location.reload();
         });
     };
+
+    $rootScope.$on('$locationChangeStart', function (event) {
+        showSpinner();
+        $("html, body").stop().animate({scrollTop: 0}, '100', 'swing');
+    });
 });
 
 app.controller('indexCtrl', function ($scope, $location, $rootScope, $routeParams) {
@@ -69,9 +77,86 @@ app.controller('paymentsCtrl', function ($scope, $location, $rootScope, $routePa
 });
 app.controller('profileCtrl', function ($scope, $location, $rootScope, $routeParams) {
     $rootScope.title = 'profile';
+    if (!$rootScope.currentUser)
+        $location.path('/');
 
-    hideSpinner();
+    var query = new Parse.Query(Parse.User);
+    query.include('accountType');
+    query.get($rootScope.currentUser.id, {
+        success: function (user) {
+            $scope.accountType = user.get('accountType');
+            $scope.$apply();
+            hideSpinner();
+        },
+        error: function (user, error) {
+            hideSpinner();
+            alert("Error: " + error.code + " " + error.message);
+        }
+    });
 });
+app.controller('editProfileCtrl', function ($scope, $location, $rootScope, $routeParams) {
+    $rootScope.title = 'Edit Profile';
+    if (!$rootScope.currentUser)
+        $location.path('/');
+
+    var AccountTypes = Parse.Object.extend("AccountTypes");
+    var query = new Parse.Query(AccountTypes);
+    query.find({
+        success: function (results) {
+            $scope.accounts = angular.copy(results);
+            var user = $rootScope.currentUser;
+            $scope.username = user.get('username');
+            $scope.fullname = user.get('name');
+            $scope.address = user.get('address');
+            $scope.email = user.get('email');
+            $scope.accountType = $scope.accounts.indexOf($scope.accounts.find(function (e) {
+                return e.id == user.get('accountType').id;
+            }));
+            $scope.$apply();
+            hideSpinner();
+        },
+        error: function (error) {
+            alert("Error: " + error.code + " " + error.message);
+            hideSpinner();
+        }
+    });
+
+    $scope.save = function () {
+        var user = $rootScope.currentUser;
+        if ($scope.username != user.get('username'))
+            user.set("username", $scope.username);
+        if ($scope.password)
+            user.set("password", $scope.password);
+        if ($scope.email != user.get('email'))
+            user.set("email", $scope.email);
+        if ($scope.fullname != user.get('name'))
+            user.set('name', $scope.fullname);
+        if ($scope.address != user.get('address'))
+            user.set('address', $scope.address);
+        if ($scope.accounts[$scope.accountType].id != user.get('accountType').id)
+            user.set('accountType', $scope.accounts[$scope.accountType]);
+
+        var fileUploadControl = $("#pic")[0];
+        if (fileUploadControl.files.length > 0) {
+            var file = fileUploadControl.files[0];
+            var name = "photo.jpg";
+
+            var parseFile = new Parse.File(name, file);
+            user.set('pic', parseFile);
+        }
+        showSpinner();
+        user.save({
+            success: function (user) {
+                location.reload();
+            },
+            error: function (user, error) {
+                hideSpinner();
+                alert("Error: " + error.code + " " + error.message);
+            }
+        });
+    };
+});
+
 app.controller('servicesCtrl', function ($scope, $location, $rootScope, $routeParams) {
     $rootScope.title = 'services';
 
@@ -113,7 +198,20 @@ app.controller('signupCtrl', function ($scope, $location, $rootScope, $routePara
 
     if ($rootScope.currentUser)
         $location.path('/');
-    hideSpinner();
+    var AccountTypes = Parse.Object.extend("AccountTypes");
+    var query = new Parse.Query(AccountTypes);
+    query.find({
+        success: function (results) {
+            $scope.accounts = angular.copy(results);
+            $scope.accountType = 0;
+            $scope.$apply();
+            hideSpinner();
+        },
+        error: function (error) {
+            alert("Error: " + error.code + " " + error.message);
+            hideSpinner();
+        }
+    });
 
     $scope.signup = function () {
         if (!$scope.username || !$scope.password || !$scope.email) {
@@ -124,6 +222,18 @@ app.controller('signupCtrl', function ($scope, $location, $rootScope, $routePara
         user.set("username", $scope.username);
         user.set("password", $scope.password);
         user.set("email", $scope.email);
+        user.set('name', $scope.fullname);
+        user.set('address', $scope.address);
+        user.set('accountType', $scope.accounts[$scope.accountType]);
+
+        var fileUploadControl = $("#pic")[0];
+        if (fileUploadControl.files.length > 0) {
+            var file = fileUploadControl.files[0];
+            var name = "photo.jpg";
+
+            var parseFile = new Parse.File(name, file);
+            user.set('pic', parseFile);
+        }
         showSpinner();
         user.signUp(null, {
             success: function (user) {
